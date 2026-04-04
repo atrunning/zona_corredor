de18b2bbb6d8f472f3fe7f2d8e034949d79caeed
atrunning <atrunning20@gmail.com>
2026-04-04 00:12:52 -0300

    Update database connection details in db.py

--- db.py
+++ db.py
@@ -2,8 +2,14 @@ import mysql.connector
 
 def get_db_connection():
     return mysql.connector.connect(
-        host="localhost",
+        host="junction.proxy.rlwy.net",
+        port=39867,
         user="root",
-        password="Jdro3807",
-        database="atrunning"
+        password="38801233",
+        database="railway"
     )
+
+if __name__ == "__main__":
+    conn = get_db_connection()
+    print("CONECTADO 🚀")
+    conn.close()
